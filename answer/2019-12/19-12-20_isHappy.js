d1ab7b94e7b5b74a3a07190f0addec6b7a0d3862
zouxiaomingya <13870939865@163.com>
2019-12-20 22:36:07 +0800

    feat: isHappy

---
+++ answer/2019-12/19-12-20_isHappy.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  function getSum(n){
+      let sum = 0;
+      for(let i = 0; i < n.toString().length; i++ ){
+          sum += n.toString()[i]**2;
+      }
+      return sum;
+  }
+  while(n !== 1){
+      if([4,16,37,58,89,145,42,20].indexOf(n) !== -1) break;
+      n = getSum(n)
+  }
+  
+  return n === 1 ? true : false;
+};
