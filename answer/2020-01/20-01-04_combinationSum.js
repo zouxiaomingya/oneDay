1b48d99717923dddfd5206cd97a628a082d97832
zouxiaomingya <13870939865@163.com>
2020-01-05 13:47:29 +0800

    feat

--- answer/2020-01/20-01-04_combinationSum.js
+++ answer/2020-01/20-01-04_combinationSum.js
@@ -1,39 +1,27 @@
-// 求 10 进制 转二进制 后包含多少个 1 的个数
-// 比如 13 -> 1101 有 3个1 return 3
-const n = num => {
-  let count = 0;
-  while (num) {
-    num &= num - 1;
-    count++;
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+  // 去重，在顺排序
+  const newCandidates = [...new Set(candidates)].sort((a, b) => a - b)
+  return fn(newCandidates, target)
+}
+var fn = function (candidates, target, start = 0) {
+  const result = []
+  for (let i = start; i < candidates.length; i++) {
+    // 由于是顺序 如果当前的值都大雨目标值，那么直接 跳出循环；
+    if (target < candidates[i]) break;
+    if (target === candidates[i]) result.push([target]);
+    // 返回值是二维数组
+    // 给出 i 是为了防止 回溯 会有重复的
+    const arr = fn(candidates, target - candidates[i], i)
+    // 把当前 index === i 的元素放每个数组前面
+    arr.forEach(item => {
+        item.unshift(candidates[i])
+        result.push(item)
+    })
   }
-  return count;
-};
-
-// 参数依次为目标数组、选取元素数目、目标和
-const search = (arr, count, sum) => {
-  let len = arr.length,
-    bit = 1 << len,
-    res = [];
-
-  // 遍历所有的选择情况
-  for (let i = 1; i < bit; i++) {
-    if (n(i) === count) {
-      let s = 0,
-        temp = [];
-      // 每一种满足个数为 N 的选择情况下，继续判断是否满足 和为 M
-      for (let j = 0; j < len; j++) {
-        // 建立映射，找出选择位上的元素
-        if ((i & (1 << j)) !== 0) {
-          s += arr[j];
-          temp.push(arr[j]);
-        }
-      }
-      // 如果这种选择情况满足和为 M
-      if (s === sum) {
-        res.push(temp);
-      }
-    }
-  }
-
-  return res;
+  return result
 };
