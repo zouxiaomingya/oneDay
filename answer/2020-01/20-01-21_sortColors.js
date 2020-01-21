0ac566c4b545dc4136af0bd7bb627a35d64e9532
zouxiaomingya <13870939865@163.com>
2020-01-21 21:24:55 +0800

    answer sortColor

---
+++ answer/2020-01/20-01-21_sortColors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+  let p0 = 0, curr = 0;
+  // 对于所有 idx > k : nums[idx > k] = 2
+  let p2 = nums.length - 1;
+
+  let tmp;
+  while (curr <= p2) {
+    if (nums[curr] == 0) {
+      // 交换第 p0个和第curr个元素
+      // i++，j++
+      tmp = nums[p0];
+      nums[p0++] = nums[curr];
+      nums[curr++] = tmp;
+    }
+    else if (nums[curr] == 2) {
+      // 交换第k个和第curr个元素
+      // p2--
+      tmp = nums[curr];
+      nums[curr] = nums[p2];
+      nums[p2--] = tmp;
+    }
+    else curr++;
+  }
+}
