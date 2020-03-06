aa565b25b77144b50d170548369bf8eb443b9b32
zouxiaomingya <13870939865@163.com>
2020-03-06 15:43:38 +0800

    answer: use math get findContinuousSequence

--- answer/2020-03/20-03-01_findContinuousSequence.js
+++ answer/2020-03/20-03-01_findContinuousSequence.js
@@ -12,11 +12,32 @@ var findContinuousSequence = function(target) {
             sum += j;
             if(sum === target){
                 result.push(new Array(j - i + 1).fill(i).map((item, index) => item + index))
-            }
+            }   
             if(sum > target) break;
         }
         i++;
     }
     return result;
 
+};
+
+// 数学算法
+// javascript 数学算法
+// 首项是 i , 末项是 j, 目标值是 x.
+// 公示 ( i + j )( j - i + 1) = 2x
+// 解得  j = Math.sqrt(2*x + i*i - i + 1/4) - 1/2
+// 如果 j 是个整数 所以该解是符合要求的
+/**
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function(target) {
+    var result = [];
+    for(let i = 1; i <= Math.ceil(target / 2); i++){
+        var j = Math.sqrt(2*target + i*i - i + 1/4) - 1/2
+        if(j % 1 === 0 ){
+            result.push(new Array(j - i + 1).fill(i).map((item, index) => item + index))
+        }
+    }
+    return result
 };
