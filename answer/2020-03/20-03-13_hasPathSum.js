786257576c9d1a5a0014ca960d28b8ffc41016b0
zouxiaomingya <13870939865@163.com>
2020-03-15 20:41:35 +0800

    answer: hasPathSum

---
+++ answer/2020-03/20-03-13_hasPathSum.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if(!root) return false;
+    if (root.left == null && root.right == null) {
+            return root.val === sum;
+        }
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
