f5f5bf47461b279530ad6f33080b56c67901d10a
zouxiaomingya <13870939865@163.com>
2020-05-04 23:32:12 +0800

    answer

--- answer/2020-05/20-05-04_permute.js
+++ answer/2020-05/20-05-04_permute.js
@@ -1,26 +1,7 @@
-var searchRange = function(nums, target) {
-  if(!nums.length) return [-1,-1];
-  let left = 0;
-  let right = nums.length -1;
-  let start = -1;
-  let end = -1;
-  while(left <=right) {
-      let mid = left + Math.floor((right -left)/2);
-      if(nums[mid] === target) {
-          start = mid;
-          end = mid 
-          while(start > left && nums[start-1] === target) {
-              start--;
-          }
-          while(end < right && nums[end+1] === target) {
-              end++;
-          }
-          return [start,end]
-      } else if(nums[mid] > target) {
-          right = mid - 1;
-      } else {
-          left = left + 1;
-      }
-  }
-  return [-1,-1]
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+
 };
