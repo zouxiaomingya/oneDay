26e48dac0fb6a093d24713dfaa98d74c31c1c56a
zouxiaomingya <13870939865@163.com>
2020-05-05 18:41:21 +0800

    answer

--- answer/2020-05/20-05-04_permute.js
+++ answer/2020-05/20-05-04_permute.js
@@ -1,7 +1,19 @@
+function backtrack(list, tempList, nums) {
+  if (tempList.length === nums.length) return list.push([...tempList]);
+  for(let i = 0; i < nums.length; i++) {
+      // 无重复数字，所以如果有相同的直接跳过
+      if (tempList.includes(nums[i])) continue;
+      tempList.push(nums[i]);
+      backtrack(list, tempList, nums);
+      tempList.pop();
+  }
+}
 /**
- * @param {number[]} nums
- * @return {number[][]}
- */
+* @param {number[]} nums
+* @return {number[][]}
+*/
 var permute = function(nums) {
-
+  const list = [];
+  backtrack(list, [], nums)
+  return list
 };
