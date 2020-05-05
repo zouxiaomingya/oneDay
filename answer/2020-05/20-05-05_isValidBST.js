f800fa7cf01ae513355071ffee3035450d9e26c8
zouxiaomingya <13870939865@163.com>
2020-05-05 19:11:35 +0800

    answer

--- answer/2020-05/20-05-05_isValidBST.js
+++ answer/2020-05/20-05-05_isValidBST.js
@@ -9,6 +9,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
 
+const tree = {
+  val: 10,
+  left: { val: 5, left: null, right: null },
+  right: {
+    val: 15,
+    left: { val: 6, left: null, right: null },
+    right: { val: 20, left: null, right: null },
+  },
 };
+var isValidBST = function (root) {
+  if (!root) return true;
+  if (root.left && root.val <= root.left.val) return false;
+  if (root.right && root.val >= root.right.val) return false;
+  return isValidBST(root.left) && isValidBST(root.right);
+};
+isValidBST(tree)
