eb5e05c0da2213879ba51f8d4ea73b93e095a521
zouxiaomingya <13870939865@163.com>
2020-05-09 00:57:40 +0800

    feat: answer

--- answer/2020-05/20-05-08_mySqrt.js
+++ answer/2020-05/20-05-08_mySqrt.js
@@ -2,6 +2,21 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-
+// 暴力解法
+var mySqrt = function (x) {
+  var re = 0;
+  while (!(re * re <= x && (re + 1) * (re + 1) > x)) {
+    re++;
+  }
+  return re;
 };
+// 牛顿迭代
+var mySqrt = function (x) {
+  if (x === 0) return 0;
+  var re = 1;
+  while (!(re * re <= x && (re + 1) * (re + 1) > x)) {
+    re = parseInt(re - (re * re - x) / (2 * re));
+  }
+  return re;
+};
+mySqrt(10);
