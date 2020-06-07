bbf62e9fea31bcd34f58477a22ce0e64fc2ca529
zouxiaomingya <13870939865@163.com>
2020-06-07 19:06:35 +0800

    answer

--- answer/2020-06/20-06-05_singleNumber.js
+++ answer/2020-06/20-06-05_singleNumber.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+var singleNumber = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+  for (var i = 2; i < nums.length; i += 3) {
+    if (nums[i] + nums[i - 1] + nums[i - 2] !== nums[i] * 3) {
+      return nums[i] ^ nums[i - 1] ^ nums[i - 2];
+    }
+  }
 
+  return nums[nums.length - 1];
 };
