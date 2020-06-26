beafaff273300d77b90703befae42ab5f6ba720b
zouxiaomingya <13870939865@163.com>
2020-06-26 09:44:17 +0800

    answer

--- answer/2020-06/20-06-26_topKFrequent.js
+++ answer/2020-06/20-06-26_topKFrequent.js
@@ -4,5 +4,15 @@
  * @return {string[]}
  */
 var topKFrequent = function(words, k) {
-
+  let map = new Map();
+  words.forEach((item)=>{
+      if(map.has(item)){
+        map.set(item,map.get(item)+1);
+      }else{
+          map.set(item,1)
+      }
+  })
+  let key = [...map].sort((a,b)=>b[1]-a[1]|| a[0].localeCompare(b[0]));
+  key = key.map(item=>item[0]);
+  return key.slice(0,k);
 };
