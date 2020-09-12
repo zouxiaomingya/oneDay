78180c15d9267692d9ab527618c00d2555bd6836
zouxiaomingya <13870939865@163.com>
2020-09-12 16:22:03 +0800

    feat

--- answer/2020-09/20-09-12_isPerfectSquare.js
+++ answer/2020-09/20-09-12_isPerfectSquare.js
@@ -1,25 +1,14 @@
 /**
- * @param {number} n
- * @return {number}
+ * @param {number} num
+ * @return {boolean}
  */
-var nthUglyNumber = function (n) {
-  let ret = [1];
-  let num2 = 0;
-  let num3 = 0;
-  let num5 = 0;
-  while (n > 0) {
-    let temp = Math.min(ret[num2] * 2, ret[num3] * 3, ret[num5] * 5);
-    ret.push(temp);
-    if (ret[num2] * 2 == temp) {
-      num2++;
-    }
-    if (ret[num3] * 3 == temp) {
-      num3++;
-    }
-    if (ret[num5] * 5 == temp) {
-      num5++;
-    }
-    n--;
+var isPerfectSquare = function(num) {
+  let temp = false;
+  for(let i = 0; i < num / 2 + 1; i++){
+      if(i * i === num){
+          temp = true;
+          break;
+      }
   }
-  return ret[ret.length - 2];
+  return temp;
 };
