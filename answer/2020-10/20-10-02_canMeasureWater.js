d18fea04ebb5b5bcf1432a772f19ed3fbf1fdf1d
zouxiaomingya <13870939865@163.com>
2020-10-02 22:09:52 +0800

    feat

--- answer/2020-10/20-10-02_canMeasureWater.js
+++ answer/2020-10/20-10-02_canMeasureWater.js
@@ -4,6 +4,19 @@
  * @param {number} z
  * @return {boolean}
  */
-var canMeasureWater = function(x, y, z) {
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
 
+const canMeasureWater = (x, y, z) => {
+  if (x + y < z) {
+    return false;
+  }
+  if (x === 0 || y === 0) {
+    return z === 0 || x + y === z;
+  }
+  return z % gcd(x, y) === 0;
 };
