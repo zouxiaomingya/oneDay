9d51de94f325edf5711f1ac5fc995f7f346f9a1a
zouxiaomingya <13870939865@163.com>
2020-11-16 23:41:10 +0800

    feat: answer

--- answer/2020-11/20-11-15_arrayNesting.js
+++ answer/2020-11/20-11-15_arrayNesting.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arrayNesting = function(nums) {
-
+var arrayNesting = function (nums) {
+  let ans = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let start = i;
+    let count = 0;
+    while (nums[start] !== Infinity) {
+      count++;
+      let temp = start;
+      start = nums[start];
+      nums[temp] = Infinity;
+    }
+    if (count > ans) ans = count;
+  }
+  return ans;
 };
