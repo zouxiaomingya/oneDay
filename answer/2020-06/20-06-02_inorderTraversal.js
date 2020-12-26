efa453909acafb8b1fb1704bf7ff22d9552bcfd3
zouxiaomingya <13870939865@163.com>
2020-12-27 02:12:30 +0800

    feat: answer

--- answer/2020-06/20-06-02_inorderTraversal.js
+++ answer/2020-06/20-06-02_inorderTraversal.js
@@ -1,8 +1,9 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -10,5 +11,16 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-
+  const res = [];
+  const stk = [];
+  while (root || stk.length) {
+      while (root) {
+          stk.push(root);
+          root = root.left;
+      }
+      root = stk.pop();
+      res.push(root.val);
+      root = root.right;
+  }
+  return res;
 };
