1dc0b0f330335c61e6e1ed4772388e2080ed36ca
zouxiaomingya <13870939865@163.com>
2021-03-05 22:32:43 +0800

    feat:answer:videoStitching

--- answer/2021-03/21-03-04_videoStitching.js
+++ answer/2021-03/21-03-04_videoStitching.js
@@ -1,8 +1,15 @@
-/**
- * @param {number[][]} clips
- * @param {number} T
- * @return {number}
- */
-var videoStitching = function(clips, T) {
-    
+const videoStitching = (clips, T) => {
+  clips.sort((a, b) => a[0] - b[0]); // 按开始时间从小到大排序
+  const dp = new Array(T + 1).fill(101);
+  dp[0] = 0;
+  for (let i = 0; i < clips.length; i++) {
+    const [start, end] = clips[i];
+    for (let j = start + 1; j <= end; j++) {
+      dp[j] = Math.min(dp[j], dp[start] + 1);
+    }
+  }
+  if (dp[T] == 101) {
+    return -1;
+  }
+  return dp[T];
 };
