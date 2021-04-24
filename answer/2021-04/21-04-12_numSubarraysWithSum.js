1e64b9058cec39771e22936582c868bfaead841d
zouxiaomingya <13870939865@163.com>
2021-04-24 14:57:04 +0800

    answer: numSubarraysWithSum

--- answer/2021-04/21-04-12_numSubarraysWithSum.js
+++ answer/2021-04/21-04-12_numSubarraysWithSum.js
@@ -3,6 +3,30 @@
  * @param {number} S
  * @return {number}
  */
- var numSubarraysWithSum = function(A, S) {
 
+var numSubarraysWithSum = function (A, S) {
+  let len = A.length;
+  let res = 0;
+  let sum = 0;
+  let l = (r = 0);
+  while (r < len) {
+    sum += A[r];
+    while (sum > S) {
+      sum -= A[l];
+      l++;
+    }
+    if (sum === S) {
+      // 给定一个符合条件的最大窗口，要再分出一个指针来找出最多符合条件的连续子窗口
+      // l 指针不能动，为的是保证 [l,r] 是符合条件的最大窗口，这样在这个窗口下有多少个连续符合的子窗口都能找到
+      let tempL = l,
+        tempSum = sum;
+      while (tempL <= r) {
+        if (tempSum === S) res++;
+        tempSum -= A[tempL];
+        tempL++;
+      }
+    }
+    r++;
+  }
+  return res;
 };
